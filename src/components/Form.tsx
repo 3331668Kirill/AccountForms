b4bb51adfaeb5bad486100b8a3a4e8bf21c14f24
Kirill <3331668@mail.ru>
2021-12-23 20:06:44 +0300

    add some css styles

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react';
 import { useReactToPrint } from 'react-to-print';
 import { ComponentToPrint } from '../Print';
+import css from './form.module.css'
 
 
 export const Form = () => {
@@ -10,7 +11,7 @@ export const Form = () => {
     });
 
     return (
-        <div>
+        <div className={css.form}>
             <ComponentToPrint ref={componentRef} val={'act_form'}/>
             {/*<ComponentToPrint ref={componentRef} val={''}/>*/}
             <button style={{width:'150px', height: '125px', backgroundColor:'yellow'}} onClick={handlePrint}>Распечатать или сохранить в файл</button>
